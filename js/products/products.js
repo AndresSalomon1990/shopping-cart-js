66d468685fb2d68a97e8e9caf813e79a580eeda5
AndresSalomon1990 <a.salomon.1990@gmail.com>
2024-06-27 00:57:03 -0300

    implement increase/decrease product quantity func.

--- js/products/products.js
+++ js/products/products.js
@@ -3,9 +3,24 @@ class Products {
     this.products = products;
   }
 
+  increaseProductQuantity(id) {
+    console.log('increase id', id);
+    const quantityElement = document.getElementById(`quantity-${id}`);
+    const quantity = Number(quantityElement.textContent);
+    quantityElement.textContent = quantity + 1;
+  }
+
+  decreaseProductQuantity(id) {
+    const quantityElement = document.getElementById(`quantity-${id}`);
+    const quantity = Number(quantityElement.textContent);
+    if (quantity > 0) {
+      quantityElement.textContent = quantity - 1;
+    }
+  }
+
   getProductTemplate ({ id, title, price, imageSrc }) {
     return `
-      <div id="${id}" class="col-sm-6 col-lg-4">
+      <div id="product-${id}" class="col-sm-6 col-lg-4">
         <div class="card shadow">
           <img
             src="${imageSrc}"
@@ -17,11 +32,11 @@ class Products {
             <p class="fw-bold fs-5 mb-0">$ ${price}</p>
             <div class="d-flex align-items-center mb-2 gap-2">
               <span>Cantidad: </span>
-              <button type="button" class="btn btn-outline-primary btn-sm">
+              <button id="decrease-btn-${id}" type="button" class="btn btn-outline-primary btn-sm">
                 <i class="bi bi-dash-circle"></i>
               </button>
-              <span class="fw-bold">0</span>
-              <button type="button" class="btn btn-outline-primary btn-sm">
+              <span id="quantity-${id}" class="fw-bold">0</span>
+              <button id="increase-btn-${id}" type="button" class="btn btn-outline-primary btn-sm">
                 <i class="bi bi-plus-circle"></i>
               </button>
             </div>
@@ -38,7 +53,17 @@ class Products {
     const productsContainer = document.getElementById("products-container");
 
     this.products.forEach((product) => {
-      productsContainer.innerHTML += this.getProductTemplate(product);
+      productsContainer.insertAdjacentHTML("beforeend", this.getProductTemplate(product));
+
+      const increaseBtn = document.getElementById(`increase-btn-${product.id}`);
+      increaseBtn.addEventListener("click", () => {
+        this.increaseProductQuantity(product.id);
+      });
+
+      const decreaseBtn = document.getElementById(`decrease-btn-${product.id}`);
+      decreaseBtn.addEventListener("click", () => {
+        this.decreaseProductQuantity(product.id);
+      });
     });
   }
 }
